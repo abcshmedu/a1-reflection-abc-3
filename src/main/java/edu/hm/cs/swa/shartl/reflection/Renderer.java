5c786657e0a135b17c64b5e181eae6d93fa1dd96
Sebastian <sese.hartl@gmail.com>
2017-03-28 13:05:25 +0200

    current state

--- src/main/java/edu/hm/cs/swa/shartl/reflection/Renderer.java
+++ src/main/java/edu/hm/cs/swa/shartl/reflection/Renderer.java
@@ -1,6 +1,7 @@
 package edu.hm.cs.swa.shartl.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Renderer, which outputs string of @RenderMe vars.
@@ -26,12 +27,14 @@ public class Renderer {
     public String render() throws IllegalAccessException {
         final Class toRender = this.object.getClass();
         final Field[] fields = toRender.getDeclaredFields();
+        final Method[] methods = toRender.getDeclaredMethods();
         final StringBuilder builder = new StringBuilder();
         builder.append("Instance of ");
         builder.append(toRender.getCanonicalName());
         builder.append(":\n");
 
         renderFields(fields, builder);
+        renderMethods(methods, builder);
 
         System.out.println(builder.toString());
         return builder.toString();
@@ -39,25 +42,69 @@ public class Renderer {
 
     /**
      * Render fields of given class.
-     * @param fields Fields to be rendered.
+     *
+     * @param fields  Fields to be rendered.
      * @param builder For string output.
      * @throws IllegalAccessException if field can't get read via reflection.
      */
     private void renderFields(final Field[] fields, final StringBuilder builder) throws IllegalAccessException {
         for (final Field field : fields) {
             if (field.isAnnotationPresent(RenderMe.class)) {
-                field.setAccessible(true);
-                builder.append(field.getName());
-                builder.append(" (Type ");
-                if (field.getType().isPrimitive()) {
-                    builder.append(field.getType().getSimpleName());
+                final RenderMe annotation = field.getAnnotation(RenderMe.class);
+                if (annotation.with().equals("")) {
+                    field.setAccessible(true);
+                    builder.append(field.getName());
+                    builder.append(" (Type ");
+                    if (field.getType().isPrimitive()) {
+                        builder.append(field.getType().getSimpleName());
+                    } else {
+                        builder.append(field.getType().getCanonicalName());
+                    }
+                    builder.append("): ");
+                    builder.append(field.get(object).toString());
+                    builder.append('\n');
                 } else {
-                    builder.append(field.getType().getCanonicalName());
+                    renderViaReflection(annotation.with());
                 }
-                builder.append("): ");
-                builder.append(field.get(object));
-                builder.append('\n');
             }
+        }
+    }
+
+    /**
+     * Render methods of given class.
+     *
+     * @param methods Methods to be rendered.
+     * @param builder For string output.
+     * @throws IllegalAccessException if field can't get read via reflection.
+     */
+    private void renderMethods(final Method[] methods, final StringBuilder builder) throws IllegalAccessException {
+        for (final Method method : methods) {
+            if (method.isAnnotationPresent(RenderMe.class)) {
+                if (!method.getReturnType().toString().equals("void")) {
+                    builder.append(method.getName());
+                    builder.append(" returns: ");
+
+                    builder.append(method.getReturnType());
+                    builder.append('\n');
+                }
+            }
+        }
+    }
+
+    /**
+     * Renders via a custom renderer.
+     * @param canonicalName name of renderer for reflection.
+     */
+    private void renderViaReflection(final String canonicalName)  {
+        try {
+            final Class renderer = Class.forName(canonicalName);
+            renderer.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/edu/hm/cs/swa/shartl/reflection/SomeClass.java
+++ src/main/java/edu/hm/cs/swa/shartl/reflection/SomeClass.java
@@ -9,12 +9,16 @@ import java.util.Date;
 public class SomeClass {
     @RenderMe
     private int foo;
-    @RenderMe(with = "edu.hm.renderer.ArrayRenderer")
+    @RenderMe
     int[] array = {1, 2, 3,};
     @RenderMe
     private Date date = new Date(123456789);
 
     public SomeClass(int foo) {
         this.foo = foo;
+    }
+
+    @RenderMe
+    public void foo() {
     }
 }
