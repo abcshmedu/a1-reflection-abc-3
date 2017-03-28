c643e4c93fe33d53ce244ea85a47daddff35b20b
Sebastian <sese.hartl@gmail.com>
2017-03-28 10:44:04 +0200

    current version

--- src/main/java/edu/hm/cs/swa/shartl/reflection/RenderMe.java
+++ src/main/java/edu/hm/cs/swa/shartl/reflection/RenderMe.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Annotation RenderMe.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface RenderMe {
     /**
      * Currently not used.
